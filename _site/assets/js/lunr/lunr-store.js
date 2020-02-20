411ce38105ab026b3fe850338262d163b236a998
Mubai Liu <liumubai000@126.com>
2020-02-20 12:38:13 -0500

    create sample post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,10 +2,15 @@ var store = [{
         "title": "Data Wrangling Project",
         "excerpt":"H1 HeadingH2 HeadingH3 HeadingHere’s some basic text.And here’s some italicsHere’s some bold text.What about a link?Here’s a bulleted list: First item Second item Third itemHere’s a numbered list: First Second ThirdPython code block: import numpy as np def test_function(x, y): z = np.sum(x,y) return zR code block:library(tidyverse)df &lt;- read_csv(\"some_file.csv\")head(df)Here’s some...","categories": ["life"],
         "tags": ["data wrangling","data science","messy data"],
-        "url": "http://localhost:4000/life/perceptron/",
+        "url": "http://localhost:4000/Blog/perceptron/",
         "teaser":null},{
         "title": "US 2016 Election Analysis",
         "excerpt":"All Codes in GithubBackground DiscussionQ1: What makes predicting voter behavior (and thus election forecasting) a hard problem?Many reasons may lead to the difficulty of predicting the voter behavior and election forecasting. The first one may be the number of voters for the2016’s polls. While we expect that the equal number...","categories": ["tutorial"],
         "tags": ["data wrangling","data visualization","prediction","R"],
-        "url": "http://localhost:4000/tutorial/Election/",
+        "url": "http://localhost:4000/Blog/Election/",
+        "teaser":null},{
+        "title": "Sample Post",
+        "excerpt":"Overview HTML Elements Heading 1 Heading 2 Heading 3 Heading 4 Heading 5 Heading 6 Body text Blockquotes List Types Ordered Lists Unordered Lists Tables Code Snippets Buttons Notices HTML ElementsBelow is just about everything you’ll need to style in the theme. Check the source code to see the many...","categories": [],
+        "tags": ["intro","beginner","jekyll","tutorial"],
+        "url": "http://localhost:4000/Blog/Sample_post/",
         "teaser":null}]
