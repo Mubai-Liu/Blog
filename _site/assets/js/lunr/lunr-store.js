5f34d718dfb0b02ab68d26913b0e1c31c2c1f470
Mubai Liu <liumubai000@126.com>
2020-02-20 11:21:20 -0500

    update local server

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "Data Wrangling Project",
+        "excerpt":"H1 HeadingH2 HeadingH3 HeadingHere’s some basic text.And here’s some italicsHere’s some bold text.What about a link?Here’s a bulleted list: First item Second item Third itemHere’s a numbered list: First Second ThirdPython code block: import numpy as np def test_function(x, y): z = np.sum(x,y) return zR code block:library(tidyverse)df &lt;- read_csv(\"some_file.csv\")head(df)Here’s some...","categories": ["life"],
+        "tags": ["data wrangling","data science","messy data"],
+        "url": "http://localhost:4000/life/perceptron/",
+        "teaser":null},{
+        "title": "US 2016 Election Analysis",
+        "excerpt":"All Codes in GithubBackground DiscussionQ1: What makes predicting voter behavior (and thus election forecasting) a hard problem?Many reasons may lead to the difficulty of predicting the voter behavior and election forecasting. The first one may be the number of voters for the2016’s polls. While we expect that the equal number...","categories": [],
+        "tags": ["data wrangling","data visualization","prediction","R"],
+        "url": "http://localhost:4000/Election/",
+        "teaser":null}]
