5c03b3a7c9cb6438f47f415297e7705942308605
Mubai Liu <liumubai000@126.com>
2020-02-21 09:40:32 -0500

    local run

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -8,4 +8,14 @@ var store = [{
         "excerpt":"All Codes in Github Background Discussion Q1: What makes predicting voter behavior (and thus election forecasting) a hard problem? Many reasons may lead to the difficulty of predicting the voter behavior and election forecasting. The first one may be the number of voters for the2016’s polls. While we expect that...","categories": ["project"],
         "tags": ["data wrangling","data visualization","prediction","R"],
         "url": "http://localhost:4000/Election/",
+        "teaser":null},{
+        "title": "Auto Chess in Dota 2",
+        "excerpt":"This is a video uploaded by myself that playing Auto Chess a year ago. It is a really fun game to play and require some unique strategies in order to win against 7 other players. Enjoy the video.  ","categories": ["life"],
+        "tags": ["bilibili","autochess"],
+        "url": "http://localhost:4000/Autochess/",
+        "teaser":null},{
+        "title": "LG Gram Youtube Video",
+        "excerpt":"Just one example post about a Youtuber Dave Lee’s video. ","categories": ["life"],
+        "tags": ["Youtube","laptop","example"],
+        "url": "http://localhost:4000/Lightest-laptop/",
         "teaser":null}]
