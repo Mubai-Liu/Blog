cf2f6b1fc7dae08d450f7607b468467f8c95ac68
Mubai Liu <liumubai000@126.com>
2020-02-21 10:00:56 -0500

    disable toc

--- _posts/2020-02-20-Autochess.md
+++ _posts/2020-02-20-Autochess.md
@@ -3,6 +3,7 @@ title: "Auto Chess in Dota 2"
 data: 2020-02-20
 tags: [bilibili,autochess]
 categories: life
+toc: false
 ---
 
 This is a video uploaded by myself that playing Auto Chess a year ago. It is a really fun game to play and require some unique strategies in order to win against 7 other players. Enjoy the video.
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,9 @@
 var store = [{
+        "title": "Post: Gallery",
+        "excerpt":"These are gallery tests for image wrapped in &lt;figure&gt; elements. To place a gallery add the necessary YAML Front Matter: gallery: - url: /assets/images/unsplash-gallery-image-1.jpg image_path: /assets/images/unsplash-gallery-image-1-th.jpg alt: \"placeholder image 1\" title: \"Image 1 title caption\" - url: /assets/images/unsplash-gallery-image-2.jpg image_path: /assets/images/unsplash-gallery-image-2-th.jpg alt: \"placeholder image 2\" title: \"Image 2 title caption\" -...","categories": ["Post Formats"],
+        "tags": ["gallery","Post Formats","tiled"],
+        "url": "http://localhost:4000/post-gallery/",
+        "teaser":null},{
         "title": "Data Wrangling Project",
         "excerpt":"H1 Heading H2 Heading H3 Heading Here’s some basic text. And here’s some italics Here’s some bold text. What about a link? Here’s a bulleted list: First item Second item Third itemHere’s a numbered list: First Second ThirdPython code block: import numpy as np def test_function(x, y): z = np.sum(x,y)...","categories": ["Sample"],
         "tags": ["data wrangling","data science","messy data"],
