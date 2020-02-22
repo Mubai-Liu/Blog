1c135b118eb0869923f9222af094d34e6af2887c
Mubai Liu <liumubai000@126.com>
2020-02-21 23:39:00 -0500

    Update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,26 +1,26 @@
 var store = [{
         "title": "Post: Gallery",
-        "excerpt":"These are gallery tests for image wrapped in &lt;figure&gt; elements. To place a gallery add the necessary YAML Front Matter: gallery: - url: /assets/images/unsplash-gallery-image-1.jpg image_path: /assets/images/unsplash-gallery-image-1-th.jpg alt: \"placeholder image 1\" title: \"Image 1 title caption\" - url: /assets/images/unsplash-gallery-image-2.jpg image_path: /assets/images/unsplash-gallery-image-2-th.jpg alt: \"placeholder image 2\" title: \"Image 2 title caption\" -...","categories": ["Post Formats"],
+        "excerpt":"These are gallery tests for image wrapped in &lt;figure&gt; elements. To place a gallery add the necessary YAML Front Matter: gallery: - url: /assets/images/unsplash-gallery-image-1.jpg image_path: /assets/images/unsplash-gallery-image-1-th.jpg alt: \"placeholder image 1\" title: \"Image 1 title caption\" - url: /assets/images/unsplash-gallery-image-2.jpg image_path: /assets/images/unsplash-gallery-image-2-th.jpg alt: \"placeholder image 2\" title: \"Image 2 title caption\" -...","categories": ["Sample"],
         "tags": ["gallery","Post Formats","tiled"],
-        "url": "http://localhost:4000/post-gallery/",
+        "url": "http://localhost:4000/Blog/post-gallery/",
         "teaser":null},{
-        "title": "Data Wrangling Project",
+        "title": "Example Project",
         "excerpt":"H1 Heading H2 Heading H3 Heading Here’s some basic text. And here’s some italics Here’s some bold text. What about a link? Here’s a bulleted list: First item Second item Third itemHere’s a numbered list: First Second ThirdPython code block: import numpy as np def test_function(x, y): z = np.sum(x,y)...","categories": ["Sample"],
         "tags": ["data wrangling","data science","messy data"],
-        "url": "http://localhost:4000/perceptron/",
+        "url": "http://localhost:4000/Blog/perceptron/",
         "teaser":null},{
         "title": "US 2016 Election Analysis",
         "excerpt":"All Codes in Github Background Discussion Q1: What makes predicting voter behavior (and thus election forecasting) a hard problem? Many reasons may lead to the difficulty of predicting the voter behavior and election forecasting. The first one may be the number of voters for the2016’s polls. While we expect that...","categories": ["project"],
         "tags": ["data wrangling","data visualization","prediction","R"],
-        "url": "http://localhost:4000/Election/",
+        "url": "http://localhost:4000/Blog/Election/",
         "teaser":null},{
         "title": "Auto Chess in Dota 2",
         "excerpt":"This is a video uploaded by myself that playing Auto Chess a year ago. It is a really fun game to play and require some unique strategies in order to win against 7 other players. Enjoy the video.  ","categories": ["life"],
         "tags": ["bilibili","autochess"],
-        "url": "http://localhost:4000/Autochess/",
+        "url": "http://localhost:4000/Blog/Autochess/",
         "teaser":null},{
         "title": "LG Gram Youtube Video",
         "excerpt":"Just one example post about a Youtuber Dave Lee’s video. ","categories": ["life"],
         "tags": ["Youtube","laptop","example"],
-        "url": "http://localhost:4000/Lightest-laptop/",
+        "url": "http://localhost:4000/Blog/Lightest-laptop/",
         "teaser":null}]
