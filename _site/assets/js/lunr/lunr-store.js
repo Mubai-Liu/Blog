2b26a5fcc2d9c36ebd5c292f74581967a6a3cf96
Mubai Liu <liumubai000@126.com>
2020-10-15 19:49:22 -0400

    site

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,36 +2,36 @@ var store = [{
         "title": "Example Project",
         "excerpt":"H1 Heading H2 Heading H3 Heading Here’s some basic text. And here’s some italics Here’s some bold text. What about a link? Here’s a bulleted list: First item Second item Third item Here’s a numbered list: First Second Third Python code block: import numpy as np def test_function(x, y): z...","categories": ["Sample"],
         "tags": ["data wrangling","data science","messy data"],
-        "url": "http://localhost:4000/sample/2018/01/28/Sample.html",
+        "url": "https://mubai-liu.github.io/sample/2018/01/28/Sample.html",
         "teaser": null
       },{
         "title": "US 2016 Election Analysis",
         "excerpt":"Background Discussion What makes predicting voter behavior (and thus election forecasting) a hard problem? Many reasons may lead to the difficulty of predicting the voter behavior and election forecasting. The first one may be the number of voters for the 2016’s polls. While we expect that the equal number of...","categories": ["project"],
         "tags": ["data wrangling","data visualization","prediction","R"],
-        "url": "http://localhost:4000/project/2018/06/11/Election.html",
+        "url": "https://mubai-liu.github.io/project/2018/06/11/Election.html",
         "teaser": null
       },{
         "title": "Auto Chess in Dota 2",
         "excerpt":"This is a video uploaded by myself that playing Auto Chess a year ago. It is a really fun game to play and require some unique strategies in order to win against 7 other players. Enjoy the video.     ","categories": ["life"],
         "tags": ["bilibili","autochess"],
-        "url": "http://localhost:4000/life/2020/02/20/Autochess.html",
+        "url": "https://mubai-liu.github.io/life/2020/02/20/Autochess.html",
         "teaser": null
       },{
         "title": "LG Gram Youtube Video",
         "excerpt":"   Just one example post about a Youtuber Dave Lee’s video.      ","categories": ["Sample"],
         "tags": ["Youtube","laptop","example"],
-        "url": "http://localhost:4000/sample/2020/02/20/Lightest-laptop.html",
+        "url": "https://mubai-liu.github.io/sample/2020/02/20/Lightest-laptop.html",
         "teaser": null
       },{
         "title": "I'm starting to build a blog!",
         "excerpt":"I’ve never thought I would have such huge amount of interests to start a blog, but here I am. It is apparently a low reward, high cost job to do, and kept me away from doing so since the day I was born. I also tried to building the website...","categories": ["life"],
         "tags": ["Personal"],
-        "url": "http://localhost:4000/life/2020/02/24/Firstpost.html",
+        "url": "https://mubai-liu.github.io/life/2020/02/24/Firstpost.html",
         "teaser": null
       },{
         "title": "Welcome to Jekyll!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
-        "url": "http://localhost:4000/jekyll/update/2020/10/14/welcome-to-jekyll.html",
+        "url": "https://mubai-liu.github.io/jekyll/update/2020/10/14/welcome-to-jekyll.html",
         "teaser": null
       }]
