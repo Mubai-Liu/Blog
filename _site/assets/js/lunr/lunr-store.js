9a7cc026bd7bff0765175c3f149ef2f90acc0415
Mubai Liu <liumubai000@126.com>
2020-10-14 22:54:22 -0400

    Delete gallery

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,10 +1,4 @@
 var store = [{
-        "title": "Post: Gallery",
-        "excerpt":"These are gallery tests for image wrapped in &lt;figure&gt; elements. To place a gallery add the necessary YAML Front Matter: gallery: - url: /assets/images/unsplash-gallery-image-1.jpg image_path: /assets/images/unsplash-gallery-image-1-th.jpg alt: \"placeholder image 1\" title: \"Image 1 title caption\" - url: /assets/images/unsplash-gallery-image-2.jpg image_path: /assets/images/unsplash-gallery-image-2-th.jpg alt: \"placeholder image 2\" title: \"Image 2 title caption\" -...","categories": ["Sample"],
-        "tags": ["gallery","Post Formats","tiled"],
-        "url": "http://localhost:4000/sample/2010/09/09/post-gallery.html",
-        "teaser": null
-      },{
         "title": "Example Project",
         "excerpt":"H1 Heading H2 Heading H3 Heading Here’s some basic text. And here’s some italics Here’s some bold text. What about a link? Here’s a bulleted list: First item Second item Third item Here’s a numbered list: First Second Third Python code block: import numpy as np def test_function(x, y): z...","categories": ["Sample"],
         "tags": ["data wrangling","data science","messy data"],
